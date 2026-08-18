52faee94c3515183248981269805d42a0768861a
monarchythe <nigam.monarchy@gmail.com>
2026-08-18 16:06:36 -0500

    we created the test files and ran dbt test, then we created the dbt docs and lineage

--- zomato/models/marts/dim_customer.sql
+++ zomato/models/marts/dim_customer.sql
@@ -1,11 +1,17 @@
-with spine as (
-    select dateadd(day, seq4(), '2024-01-01'::date) as date_day 
-    from 
-    table(generator(rowcount=>1200))
-    )
-select date_day, 
-year(date_day) as year, 
-month(date_day) as month, 
-monthname(date_day) as month_name,
-dayname(date_day) as day_name, (dayofweekiso(date_day)>=6) as is_weekend
-from spine where date_day <= '2026-12-31' 
+select
+customer_id,
+customer_name,
+email,
+age,
+CASE WHEN age < 25 then 'Gen Z'
+     WHEN age < 40 then 'Millennial'
+     WHEN age < 55 then 'Gen X'
+     WHEN age is null then 'Unknown'
+     ELSE 'Boomer' END as age_segment,
+gender,
+marital_status,
+occupation,
+income_band,
+education,
+family_size
+from {{ ref('stg_users') }}
